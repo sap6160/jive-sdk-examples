124d541d994f57cf17f294a99a5a637fffddbaf2
Aron Racho <aron.racho@jivesoftware.com>
2013-03-27 00:07:48 -0700

    Cleaup

--- basicexample/app.js
+++ basicexample/app.js
@@ -76,7 +76,6 @@ var configureApp = function (data) {
 };
 
 var startServer = function () {
-    console.log("in startServer");
     http.createServer(app).listen(app.get('port'), function () {
         console.log("Express server listening on port " + app.get('port'));
     });
